76ef9c5c23b2f66658dcb0251dc4d4e877b344c5
sega <sshunter2013@gmail.com>
2025-10-16 16:58:23 +0700

    Revert "Add gh-pages deployment script and update public path in webpack configuration"
    This reverts commit 90c4a7c4b586b20cadca198a24bb03626a995291.

--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,6 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/halaman-profil/',
     clean: true,
   },
   module: {
